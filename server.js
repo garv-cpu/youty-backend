234ce845b34fe2ef3aefba188625657abbf5c29e
thinkand-port <your@email.com>
2025-05-16 11:22:37 +0530

    changes

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 job.start();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "https://youty-backend.onrender.com", // your frontend domain
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
 app.use(express.json());
 app.use("/", downloadRoute);
 
